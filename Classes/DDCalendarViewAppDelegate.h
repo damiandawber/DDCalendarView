715d728eb9f8f5fc84cf3b0a68a283250ea3b7e8
Damian Dawber <loonychune@gmail.com>
2010-12-27 19:17:26 +0000

    Created project and added .gitignore

---
+++ Classes/DDCalendarViewAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  DDCalendarViewAppDelegate.h
+//  DDCalendarView
+//
+//  Created by Damian Dawber on 27/12/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDCalendarViewAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DDCalendarView
+//
+//  Created by Damian Dawber on 27/12/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
